a140695c500997d4d2724a9ac601cd43a9e767e8
Chatrasal Jadhav <aryanjadhav707@gmail.com>
2026-05-01 22:34:51 +0530

    Fruits in two baskets

---
+++ Arrays/Fruits Into Basket.cpp
@@ -0,0 +1,33 @@
+You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+Given the integer array fruits, return the maximum number of fruits you can pick.
+ class Solution {
+public:
+    int totalFruit(vector<int>& s) {
+        int n=s.size();
+        int maxi=INT_MIN;
+        int l=0;
+        int r=0;
+        map<int,int> mpp;
+        while(r<n){
+          mpp[s[r]]++;
+          if(mpp.size()>2){
+          if(mpp.size()>2){
+                mpp[s[l]]--;
+                if(mpp[s[l]]==0){
+                    mpp.erase(s[l]);
+                }
+                l++;
+          }
+          }
+          if(mpp.size()<=2){
+            maxi=max(maxi,r-l+1);
+          }
+          r++;
+        }
+        return maxi;
+    }
+}; 
